44f53d5558786b4420156fea38bcf84d1a2b7889
guirra-byte <guirramatheus1@gmail.com>
2024-06-17 15:50:14 -0300

    fix: register commands to prepare environment to run application

--- docker-container.commands.bash
+++ docker-container.commands.bash
@@ -10,14 +10,3 @@ $ docker run \
 
 # SOLVING ERROR: [permission denied while trying to connect to the Docker daemon socket at unix]
 $ sudo chmod 666 /var/run/docker.sock
-
-# LOCALSTACK COMMANDS
-$ localstack start
-$ aws --endpoint-url=http://localhost:4566 s3 mb s3://danieldcs
-# aws - aws-cli command
-# --endpoint-url=http://localhost:${Localstack Port} 
-# mb - (Make Bucket)
-# s3://${Bucket Name}
-$ aws --endpoint-url=http://localhost:4566 s3 ls
-# ls - List created buckets
-$ npm install aws-sdk
