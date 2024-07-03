7f727b186a6a639c4f130068e6f64ac262e93f20
guirra-byte <guirramatheus1@gmail.com>
2024-07-03 17:51:22 -0300

    fix: define dir to save only transcriptions output files

--- src/config/path.config.mjs
+++ src/config/path.config.mjs
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const filePath = (pattern, file_id) => {
-  return path.join(__dirname, `../output/tmp/${pattern}-${file_id}`);
+  return path.join(__dirname, `../output/tmp/archives/${pattern}-${file_id}`);
 }
 
 const chunkingOutPath = path.join(__dirname, '../output/tmp/videos/chunks');
